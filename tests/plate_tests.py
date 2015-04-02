d4e5de1c2da1aba7ef4a1f26d968dfcd5ad8aa97
Yoav Ram <yoavram+github@gmail.com>
2015-04-02 10:32:27 +0300

    matrix->array

--- tests/plate_tests.py
+++ tests/plate_tests.py
@@ -11,7 +11,7 @@ fname = 'tests/plate.csv'
 
 class PlateTest(unittest.TestCase):
 	def setUp(self):
-		self.matrix = np.array([[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		self.array = np.array([[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 								[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 								[0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0],
 								[0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -22,17 +22,17 @@ class PlateTest(unittest.TestCase):
 
 
 	def test_init(self):
-		plate = Plate(self.matrix)
-		self.assertTrue((plate._matrix == self.matrix).all())
+		plate = Plate(self.array)
+		self.assertTrue((plate._array == self.array).all())
 
 
 	def test_from_csv(self):
 		plate  = Plate.from_csv(fname)
-		self.assertTrue((plate._matrix == self.matrix).all())
+		self.assertTrue((plate._array == self.array).all())
 
 
 	def test_to_csv(self):
-		plate = Plate(self.matrix)
+		plate = Plate(self.array)
 		output = tempfile.NamedTemporaryFile(delete=False)
 		plate.to_csv(output.name)
 		self.assertTrue(filecmp.cmp(output.name, fname))		
@@ -40,10 +40,10 @@ class PlateTest(unittest.TestCase):
 
 
 	def test_to_array(self):
-		plate  = Plate(self.matrix)
+		plate  = Plate(self.array)
 		arr = plate.to_array()
-		self.assertTrue((arr == self.matrix).all())
-		self.assertFalse(arr is self.matrix)
+		self.assertTrue((arr == self.array).all())
+		self.assertFalse(arr is self.array)
 
 
 if __name__ == '__main__':
