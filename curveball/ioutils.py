0aad0b321a16dce3595434d97dcba642f1080451
Yoav Ram <yoavram+github@gmail.com>
2015-05-03 15:52:33 +0300

    first version of mat file reader

--- curveball/ioutils.py
+++ curveball/ioutils.py
@@ -9,6 +9,10 @@
 # Copyright (c) 2015, Yoav Ram <yoavram+github@gmail.com>
 import xlrd
 import pandas as pd
+from string import ascii_uppercase
+
+
+MAT_VERSION = '1.0'
 
 
 def read_tecan_xlsx(filename, label, sheet=None, max_time=None, plate=None):
@@ -99,3 +103,36 @@ def read_tecan_xlsx(filename, label, sheet=None, max_time=None, plate=None):
             lbli = '_' + lbli
             df = pd.merge(df, dfi, on=('Cycle Nr.','Well','Row','Col','Strain','Color'), suffixes=(lbl,lbli))
         return df
+
+
+    def read_tecan_mat(filename, time_label='tps', value_label='plate_mat', value_name='OD', plate_width=12, max_time=None, plate=None):
+        """Reads growth measurements from a Tecan mat file.
+        """
+        mat = loadmat(filename, appendmat=True)
+        if mat['__version__'] != MAT_VERSION:
+            print "Warning: expected mat file version %s but got %s" % (MAT_VERSION, mat['__version__'])
+        t = mat[time_label].
+        t = t.reshape(max(t.shape))
+        y = mat[value_label]
+        assert y.shape[1] == t.shape[0]
+
+        df = pd.DataFrame(y.T, columns=np.arange(y.shape[0]) + 1)
+        df['Time'] = t / 3600.
+        df['Cycle Nr.'] = np.arange(1, 1 + len(t))
+        df = pd.melt(df, id_vars=('Cycle Nr.', 'Time'), var_name='Well', value_name=value_name)
+        df['Well'] = map(lambda w: ascii_uppercase[(int(w)-1)/plate_width] + str(w%plate_width if w%plate_width > 0 else plate_width), df['Well'])
+        df['Row'] = map(lambda w: w[0], df['Well'])
+        df['Col'] = map(lambda w: int(w[1:]), df['Well'])
+        
+        if plate is None:
+            df['Strain'] = 0
+            df['Color'] = '#000000'
+        else:
+            df = pd.merge(df, plate, on=('Row','Col'))
+        if not max_time:
+            max_time = df.Time.max()
+        df = df[df.Time < max_time]       
+        return df
+
+
+    
