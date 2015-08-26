e6982d305bdcbc891cb181e862de058f4beeefed
Yoav Ram <yoavram+github@gmail.com>
2015-08-26 07:25:36 +0300

    fixed # args in call to sample_params

--- tests/test_models.py
+++ tests/test_models.py
@@ -499,7 +499,7 @@ class SamplingTestCase(TestCase):
         df = randomize_data(logistic_ode)
         params = curveball.models.logistic_model.make_params(r=0.1, y0=df.OD.min(), K=df.OD.max())
         model_fit = curveball.models.logistic_model.fit(data=df.OD, t=df.Time, params=params)
-        sample_params = curveball.models.sample_params(df, model_fit, 100)
+        sample_params = curveball.models.sample_params(model_fit, 100)
         self.assertIsNotNone(sample_params)
         self.assertEquals(sample_params.shape, (100, 3))
 
