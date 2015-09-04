ba01ae8b59443c5224f77e70c1ea314ddea0b053
Yoav Ram <yoavram+github@gmail.com>
2015-09-04 10:09:25 +0300

    following click tutorial, just hello world

---
+++ scripts/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of curveball.
+# https://github.com/yoavram/curveball
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/MIT-license
+# Copyright (c) 2015, Yoav Ram <yoav@yoavram.com>
+import curveball
