8521bda85b27a634ee55faf7fe2442a2d838a97a
Yoav Ram <yoavram+github@gmail.com>
2015-09-30 14:02:37 +0300

    better use of setUp, test plate with the plate_file option, basic file process test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -10,8 +10,9 @@
 from unittest import TestCase, main
 from nose.plugins.skip import SkipTest
 import os
-# See reference on testing Click applications: http://click.pocoo.org/5/testing/
-from click.testing import CliRunner
+import shutil
+import pkg_resources
+from click.testing import CliRunner # See reference on testing Click applications: http://click.pocoo.org/5/testing/
 from curveball.scripts import cli
 import curveball
 
@@ -23,7 +24,7 @@ class SimpleTestCase(TestCase):
 	_multiprocess_can_split_ = True
 
 	def setUp(self):
-		pass
+		self.runner = CliRunner()
 
 
 	def tearDown(self):
@@ -31,14 +32,12 @@ class SimpleTestCase(TestCase):
 
 
 	def test_help(self):
-		runner = CliRunner()
-		result = runner.invoke(cli.cli, ['--help'])
+		result = self.runner.invoke(cli.cli, ['--help'])
 		self.assertEquals(result.exit_code, 0)
 
 
 	def test_version(self):
-		runner = CliRunner()
-		result = runner.invoke(cli.cli, ['--version'])
+		result = self.runner.invoke(cli.cli, ['--version'])
 		self.assertEquals(result.exit_code, 0)
 		self.assertIn(curveball.__version__, result.output)
 
@@ -47,20 +46,56 @@ class PlateTestCase(TestCase):
 	_multiprocess_can_split_ = True
 
 
+	def setUp(self):
+		self.runner = CliRunner()
+
 	def test_default_plate(self):
-		runner = CliRunner()
-		result = runner.invoke(cli.cli, ['plate'])
+		result = self.runner.invoke(cli.cli, ['plate'])
 		self.assertEquals(result.exit_code, 0)
 		newlines = result.output.count("\n")
 		self.assertEquals(newlines, 98) # 96 wells, 1 header, 1 empty line
 
 
+	def test_non_default_plate(self):
+		result = self.runner.invoke(cli.cli, ['plate', '--plate_file=G-RG-R.csv'])
+		self.assertEquals(result.exit_code, 0)
+		newlines = result.output.count("\n")
+		self.assertEquals(newlines, 98) # 96 wells, 1 header, 1 empty line
+
+
+def setup_with_context_manager(testcase, cm):
+    """Use a contextmanager to setUp a test case.
+    See http://nedbatchelder.com/blog/201508/using_context_managers_in_test_setup.html
+    """
+    val = cm.__enter__()
+    testcase.addCleanup(cm.__exit__, None, None, None)
+    return val
+
+
 class AnalysisTestCase(TestCase):
 	_multiprocess_can_split_ = True
+	filename = 'Tecan_280715.xlsx'
+
+
+	def setUp(self):
+		self.runner = CliRunner()
+		self.ctx = setup_with_context_manager(self, self.runner.isolated_filesystem())
+		src = pkg_resources.resource_filename('data', self.filename)
+		dst = os.path.join('data', self.filename)
+		os.makedirs(dst)
+		shutil.copy(src, dst)		
+		self.filepath = dst		
+		self.assertTrue(os.path.exists(self.filepath))
+		self.assertTrue(os.path.exists(os.path.join(os.getcwd(), self.filepath)))
+
+
+	def tearDown(self):
+		pass
 
 
 	def test_process_file(self):
-		pass
+		result = self.runner.invoke(cli.cli, ['--no-plot', '--no-verbose', '--no-prompt', 'analyse', self.filepath, '--plate_file=G-RG-R.csv', '--ref_strain=G'])
+		self.assertEquals(result.exit_code, 0, result.output)
 
 
 if __name__ == '__main__':
