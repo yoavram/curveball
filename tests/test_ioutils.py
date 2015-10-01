74a690707f6bdd5ad9e61bd025a2402f5d11e0dd
Yoav Ram <yoavram+github@gmail.com>
2015-10-01 12:57:48 +0300

    remove blank line

--- tests/test_ioutils.py
+++ tests/test_ioutils.py
@@ -7,7 +7,6 @@
 # Licensed under the MIT license:
 # http://www.opensource.org/licenses/MIT-license
 # Copyright (c) 2015, Yoav Ram <yoav@yoavram.com>
-
 from unittest import TestCase, main
 import tempfile
 import os
