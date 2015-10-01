78d1f615185001b9858638904a0f343769eedd60
Yoav Ram <yoavram+github@gmail.com>
2015-10-01 15:30:59 +0300

    try setting mpl backend inside python instead of via config file

--- tests/test_mpl.py
+++ tests/test_mpl.py
@@ -9,6 +9,7 @@
 # Copyright (c) 2015, Yoav Ram <yoav@yoavram.com>
 from unittest import TestCase, main
 import matplotlib
+matplotlib.use('Agg')
 import os
 
 
@@ -19,4 +20,3 @@ class MplTestCase(TestCase):
 	def test_mpl(self):
 		if CI:
 			self.assertEquals(matplotlib.rcParams['backend'].lower(), 'agg')
-		
