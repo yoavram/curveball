84d5e8b7ecf367d6766219f86d8514cd24e06d07
Yoav Ram <yoavram+github@gmail.com>
2015-10-07 14:23:10 +0300

    added license

--- examples/example_competition.py
+++ examples/example_competition.py
@@ -1,4 +1,12 @@
-import pandas as pd
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of curveball.
+# https://github.com/yoavram/curveball
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/MIT-license
+# Copyright (c) 2015, Yoav Ram <yoav@yoavram.com>import pandas as pd
 import curveball
 
 if __name__ == '__main__':
