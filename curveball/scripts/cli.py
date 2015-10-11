f3704e9c57193ce2201ff9955cccc08b5dffb97b
Yoav Ram <yoavram+github@gmail.com>
2015-10-11 21:02:21 +0300

    print handler only with verbose

--- curveball/scripts/cli.py
+++ curveball/scripts/cli.py
@@ -248,7 +248,7 @@ def _process_file(filepath, plate, blank_strain, ref_strain, max_time):
 	"""
 	results = []	
 	fn,ext = os.path.splitext(filepath)
-	click.secho("file: {0}\nhandler: {1}\n".format(filepath, ext), fg='red')
+	echo_info("file: {0}\nhandler: {1}\n".format(filepath, ext))
 	handler = file_extension_handlers.get(ext)
 	if  handler == None:
 		echo_info("No handler found for file {0}".format(click.format_filename(filepath)))
