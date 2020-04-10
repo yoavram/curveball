57799a7c02c9d09c6aafdf452a83dddaa0d2cc6c
Thomas Gorochowski <tom@chofski.co.uk>
2020-04-10 12:42:46 +0100

    Updated issue with values
    values should have infact been to_numpy.

--- curveball/models.py
+++ curveball/models.py
@@ -856,8 +856,8 @@ def cooks_distance(df, model_fit, use_weights=True):
     
     for well in wells:    
         _df = df[df.Well != well]
-        time = _df.Time.values()
-        OD = _df.OD.values()
+        time = _df.Time.to_numpy()
+        OD = _df.OD.to_numpy()
         weights =  calc_weights(_df) if use_weights else None
         model_fit_i = copy.deepcopy(model_fit)
         model_fit_i.fit(data=OD, t=time, weights=weights)
@@ -999,7 +999,7 @@ def calc_weights(df, PLOT=False):
     ax : matplotlib.axes.Axes
         if the argument `PLOT` was :const:`True`, the generated axis.
     """
-    deviations = df.groupby('Time').OD.transform(lambda x: np.repeat(x.std(), len(x))).values()
+    deviations = df.groupby('Time').OD.transform(lambda x: np.repeat(x.std(), len(x))).to_numpy()
     if np.isnan(deviations).any():
         warn("NaN in deviations, can't use weights")
         weights = None
@@ -1170,11 +1170,11 @@ def fit_model(df, param_guess=None, param_min=None, param_max=None, param_fix=No
         param_fix = set()
 
     df = df.sort_values(by=['Time', 'OD'])
-    time = df.Time.values()
-    OD = df.OD.values()
+    time = df.Time.to_numpy()
+    OD = df.OD.to_numpy()
     weights =  calc_weights(df) if use_weights else None
     # TODO why should we use weights if we use the whole data set?
-    ODerr = df.groupby('Time').OD.transform(lambda x: np.repeat(x.std(), len(x))).values()
+    ODerr = df.groupby('Time').OD.transform(lambda x: np.repeat(x.std(), len(x))).to_numpy()
    
     if models is None:
         models = get_models(curveball.baranyi_roberts_model)
