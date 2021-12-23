588e104f8d2dbf4a37df24817d6d928b400ed9e9
Yoav Ram <yoav@yoavram.com>
2021-12-23 16:56:48 +0200

    dont load numpydoc

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,7 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
-    'numpydoc', # https://pypi.python.org/pypi/numpydoc    
+#    'numpydoc', # https://pypi.python.org/pypi/numpydoc    
 ]
 
 # Add any paths that contain templates here, relative to this directory.
